606181804f6187f666b4fea9116f8a164b84057e
arecofix <ezequielenrico15@gmail.com>
2025-07-08 01:25:59 -0300

    arecofix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,18 @@
 import {
   ApplicationConfig,
-  provideExperimentalZonelessChangeDetection,
-  provideZoneChangeDetection
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter, withHashLoocation } from '@angular/router';
+
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // O
-    provideExperimentalZonelessChangeDetection(),  // Opción moderna sin Zone.js
-
-    provideRouter(routes, withHashLocation()),  // Hash routing para GitHub Pages
-    provideHttpClient(withFetch()),  // HttpClient con Fetch API
-    
-    // Configuración adicional recomendada para producción:
-    { provide: 'BASE_URL', useValue: '/' },  // URL base para APIs
-  ]
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes, withHashLocation()),
+    provideHttpClient(withFetch()),
+  ],
 };
