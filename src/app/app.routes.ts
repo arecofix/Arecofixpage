e459821dba2d78c34c43a522cdfaed5a29febf2b
arecofix <ezequielenrico15@gmail.com>
2025-07-23 22:54:31 -0300

    arecofix v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Nosotros } from './pages/nosotros/nosotros';
 import { ContactoComponent } from './pages/contacto/contacto';
+import { Servicios } from './pages/servicios/servicios';
 
 export const routes: Routes = [
     {
@@ -9,6 +10,7 @@ export const routes: Routes = [
         loadChildren: () => import('@app/public/public.routes'),
     },
         {path: 'nosotros', component: Nosotros }, 
-        {path: 'contacto', component: ContactoComponent },  
+        {path: 'contacto', component: ContactoComponent },
+        {path: 'servicios', component: Servicios },
 
 ];
