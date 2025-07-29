210e77472c62b5ef4667b8efd9f2781241b1caf1
arecofix <ezequielenrico15@gmail.com>
2025-07-28 21:34:59 -0300

    arecofixv1

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,8 @@
 export const environment = {
-    production: false,
-    appName: 'Arecofix',
-    apiUrl: 'http://localhost:3000',
+  production: false, 
+  appName: 'Arecofix',
+  apiUrl: 'http://localhost:4200', // URL local para desarrollo
+  supabaseUrl: 'https://ynlwzeawbbhundpigqgy.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlubHd6ZWF3YmJodW5kcGlncWd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0ODkxMjQsImV4cCI6MjA2OTA2NTEyNH0.v2wfFPDy-RnDm8VV9yI3eiGZDTHpRAyCa7loBPQ_bsc', // Clave pública (anon key)
+  authRedirectUrl: 'http://localhost:4200/auth/callback', // Redirección post-login
 };
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  appName: 'Arecofix',
+  apiUrl: 'https://arecofix.com.ar',
+  supabaseUrl: 'https://ynlwzeawbbhundpigqgy.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlubHd6ZWF3YmJodW5kcGlncWd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0ODkxMjQsImV4cCI6MjA2OTA2NTEyNH0.v2wfFPDy-RnDm8VV9yI3eiGZDTHpRAyCa7loBPQ_bsc', // Misma clave o una diferente para prod
+  authRedirectUrl: 'https://arecofix.com.ar/auth/callback',
+};
