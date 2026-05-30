b6854c73be7a1f636a90c1b08aa19e1191627f93
arecofix <ezequielenrico15@gmail.com>
2026-05-30 13:02:48 -0300

    remove more test files with secrets

--- test_hash.py
+++
@@ -1,9 +0,0 @@
-from werkzeug.security import check_password_hash
-import sqlite3
-import os
-conn = sqlite3.connect(os.path.join(os.environ['APPDATA'], 'Arecofix', 'arecofix_local.sqlite'))
-c = conn.cursor()
-c.execute('SELECT password_hash FROM admins WHERE username=\"zaona@arecofix.com.ar\"')
-hash_str = c.fetchone()[0]
-print('Hash in DB:', hash_str)
-print('Matches zaona2026?', check_password_hash(hash_str, 'zaona2026'))
