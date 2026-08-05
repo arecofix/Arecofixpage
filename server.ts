65182049278b6671428bbb9e58a488cc6e6b50c7
arecofix <ezequielenrico15@gmail.com>
2026-08-05 16:57:46 -0300

    arex

--- server.ts
+++ server.ts
@@ -14,7 +14,16 @@ export function app(): express.Express {
   const browserDistFolder = resolve(serverDistFolder, '../browser');
   const indexHtml = join(serverDistFolder, 'index.server.html');
 
-  const commonEngine = new CommonEngine();
+  const commonEngine = new CommonEngine({
+    allowedHosts: [
+      'arecofix.com.ar',
+      'www.arecofix.com.ar',
+      'localhost',
+      '127.0.0.1',
+      'arecofix.web.app',
+      'arecofix.firebaseapp.com'
+    ]
+  });
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
